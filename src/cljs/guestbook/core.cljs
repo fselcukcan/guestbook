200d58ee4f4a6e8594d504de6d227aa80524d688
Selçuk Can <fselcukcan@gmail.com>
2026-04-10 19:11:37 +0200

    reimplemented the list.

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -5,7 +5,22 @@
             [clojure.string :as string]
             [guestbook.validation :refer [validate-message]]))
 
-(defn send-message! [fields errors]
+(defn get-messages [messages]
+  (GET "/messages"
+       {:headers {"Accept" "application/transit+json"}
+        :handler #(reset! messages (:messages %))}))
+
+(defn message-list [messages]
+  (println messages)
+  [:ul.messages
+   (for [{:keys [timestamp message name]} @messages]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]])])
+
+(defn send-message! [fields errors messages]
   (if-let [validation-errors (validate-message @fields)]
     (reset! errors validation-errors)
     (POST "/message"
@@ -16,16 +31,19 @@
        :params @fields
        :handler (fn [r]
                   (.log js/console (str "response:" r))
+                  (swap! messages conj (assoc @fields
+                                              :timestamp (js/Date.)))
+                  (reset! fields nil)
                   (reset! errors nil))
        :error-handler (fn [e]
-                        (.error js/console (str "error:" %))
+                        (.error js/console (str "error:" e))
                         (reset! errors (-> e :response :errors)))})))
 
 (defn errors-component [errors id]
   (when-let [error (id @errors)]
     [:div.notification.is-danger (string/join error)]))
 
-(defn message-form []
+(defn message-form [messages]
   (let [fields (r/atom {})
         errors (r/atom nil)]
     (fn []
@@ -52,13 +70,19 @@
                              assoc :message (-> % .-target .-value))}]]
        [:input.button.is-primary
         {:type :submit
-         :on-click #(send-message! fields errors)
+         :on-click #(send-message! fields errors messages)
          :value "comment"}]])))
 
 (defn home []
-  [:div.content>div.columns.is-centered>div.column.is-two-thirds
-   [:div.columns>div.column
-    [message-form]]])
+  (let [messages (r/atom nil)]
+    (get-messages messages)
+    (fn []
+      [:div.content>div.columns.is-centered>div.column.is-two-thirds
+       [:div.columns>div.column
+        [:h3 "Messages"]
+        [message-list messages]]
+       [:div.columns>div.column
+        [message-form messages]]])))
 
 
 (dom/render
