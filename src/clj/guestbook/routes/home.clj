f9200f82e76b84ebfcdd1650ba5fe377bfea9fd9
Selçuk Can <fselcukcan@gmail.com>
2026-04-16 01:16:08 +0200

    configuring a service api. api endpoints are seprated from html rendering ones.

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,35 +1,10 @@
 (ns guestbook.routes.home
   (:require
    [guestbook.layout :as layout]
-   [guestbook.messages :as msg]
-   [clojure.java.io :as io]
-   [guestbook.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]
-   [guestbook.validation :refer [validate-message]]))
+   [guestbook.middleware :as middleware]))
 
 (defn home-page [request]
-  (layout/render
-   request
-   "home.html"))
-
-(defn message-list [_]
-  (response/ok (msg/message-list)))
-
-(defn save-message! [{:keys [params]}]
-  (try
-    (msg/save-message! params)
-    (response/ok {:status :ok})
-    (catch Exception e
-      (let [{id :guestbook/error-id
-             errors :errors} (ex-data e)]
-        (case id
-          :validation
-          (response/bad-request {:errors errors})
-          ;;else
-          (response/internal-server-error
-           {:errors {:server-error ["Failed to save message!"]}}))))))
-
+  (layout/render request "home.html"))
 
 (defn about-page [request]
   (layout/render request "about.html"))
@@ -39,7 +14,4 @@
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/messages" {:get message-list}]
-   ["/message" {:post save-message!}]
    ["/about" {:get about-page}]])
-
