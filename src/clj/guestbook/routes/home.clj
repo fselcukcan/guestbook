bff6aba84ad7d0bc55909a97823c67a7a10fb1f6
Selçuk Can <fselcukcan@gmail.com>
2026-04-15 20:29:23 +0200

    separated code to messages namespace.

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,7 +1,7 @@
 (ns guestbook.routes.home
   (:require
    [guestbook.layout :as layout]
-   [guestbook.db.core :as db]
+   [guestbook.messages :as msg]
    [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
    [ring.util.response]
@@ -14,17 +14,21 @@
    "home.html"))
 
 (defn message-list [_]
-  (response/ok {:messages (vec (db/get-messages))}))
+  (response/ok (msg/message-list)))
 
 (defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (response/bad-request {:errors errors})
-    (try
-      (db/save-message! params)
-      (response/ok {:status :ok})
-      (catch Exception e
-        (response/internal-server-error
-         {:errors {:server-error ["Failed to save message!"]}})))))
+  (try
+    (msg/save-message! params)
+    (response/ok {:status :ok})
+    (catch Exception e
+      (let [{id :guestbook/error-id
+             errors :errors} (ex-data e)]
+        (case id
+          :validation
+          (response/bad-request {:errors errors})
+          ;;else
+          (response/internal-server-error
+           {:errors {:server-error ["Failed to save message!"]}}))))))
 
 
 (defn about-page [request]
